fb2151194853eeca5312226168b3c5a1c279c5b6
sunshineinc <inc.sunshine.coop@gmail.com>
2025-07-02 08:45:14 -0300

    Update ping.js

--- ping.js
+++ ping.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 setInterval(async () => {
   try {
-    await axios.get("https://SEU-BOT.replit.dev");
+    await axios.get("https://4293a4dc-0de5-44ca-b93c-1b85f53c1e72-00-3tgvlelsx57gr.riker.replit.dev/");
     console.log("Ping enviado com sucesso!");
   } catch (error) {
     console.error("Erro ao enviar ping:", error.message);
